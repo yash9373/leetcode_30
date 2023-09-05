866d96a570ef121f1947440bc94e6904d0041bb3
Yashodip kolhe <101787484+yash9373@users.noreply.github.com>
2023-09-05 21:42:38 +0530

    Add files via upload

---
+++ Day01/code.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        int reminder;
+        int rev=0;
+        int  x = n;
+        if (n<0)
+        {
+            return false;
+        }
+
+        while(x != 0)
+        {
+            reminder = x % 10;
+            rev = rev * 10 + reminder;
+            x = x / 10;
+        }
+        if (n == rev)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
